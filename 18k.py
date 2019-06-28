f15ad25f429e6a8a74092d9d198a90cfaac9e882
karthikmoosha <51981064+karthikmoosha@users.noreply.github.com>
2019-06-28 18:15:25 +0530

    Update 18k.py

--- 18k.py
+++ 18k.py
@@ -1,11 +1,11 @@
-M,K=map(int,input().split())
-for i in range(M+1,K):
-  sum=0
-  num=i
-  while(num>0):
-    x=num%10
-    num=num//10
-    y=x**3
-    sum=sum+y
-  if(i==sum):
-    print(sum,end=" ")
+k1,m1=map(int,input().split())
+for g in range(k1+1,m1):
+  z=0
+  s=g
+  while(s>0):
+    a=s%10
+    s=s//10
+    b=a**3
+    z=z+b
+  if(g==z):
+      print(z,end=" ")

f2b1e763c745b2b730f60011e43143c8981b2720
karthikmoosha <51981064+karthikmoosha@users.noreply.github.com>
2019-06-29 21:42:36 +0530

    Create 3k.py

---
+++ 3k.py
@@ -0,0 +1,6 @@
+MK1 = input(" ")
+
+if(MK1=='A' or MK1=='a' or MK1=='E' or MK1=='e' or Mk1=='I' or Mk1=='i' or MK1=='O' or Mk1=='o' or MK1=='U' or MK1=='u'):
+  print("Vowel")
+else:
+  print("Consonant")

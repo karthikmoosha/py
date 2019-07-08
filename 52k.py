040e358d37144b7eb81857c9899b8824af19166d
karthikmoosha <51981064+karthikmoosha@users.noreply.github.com>
2019-07-08 17:12:58 +0530

    Update and rename 52k.py to 52kk.py

--- 52k.py
+++
@@ -1,3 +0,0 @@
-k=int(input())
-m=('zero','one','two','three','four','five','six','seven','eight','nine','ten')
-print(m[k])
---
+++ 52kk.py
@@ -0,0 +1,3 @@
+k1=int(input())
+m1=('zero','one','two','three','four','five','six','seven','eight','nine','ten')
+print(m1[k1])
